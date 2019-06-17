94a27213f66319a624dc18e724d17cea468378d0
Sheng-Liang Slogar <sheng@shengslogar.com>
2019-06-16 22:07:53 -0400

    Check for SSR

--- src/lib/util.js
+++ src/lib/util.js
@@ -1,4 +1,9 @@
 export function decodeHtmlChars(input) {
+  // check for ssr
+  if (typeof document === 'undefined') {
+    return input;
+  }
+
   const textarea     = document.createElement('textarea');
   textarea.innerHTML = input;
   return textarea.value;
